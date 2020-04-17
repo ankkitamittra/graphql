a6ce1e80ea52edc648b383de388a144bcae83692
ankita <ankimitr@cisco.com>
2020-04-17 18:29:37 +0530

    other changes

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,11 +1,19 @@
 const graphql = require("graphql");
+const axios = require("axios");
 const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
 const _ = require("lodash");
 
-const users = [
-    {id:'23', firstName: 'Bill', age: 20},
-    {id:'47', firstName: 'Samantha', age: 21}
-];
+const db = {
+    users:[
+        {id:'23', firstName: 'Bill', age: 20, companyId: '1'},
+        {id:'47', firstName: 'Samantha', age: 21, companyId: '2'}
+    ],
+    companies: [
+        {id:'1', name: 'google', description: 'search'},
+        {id:'2', name: 'apple', description: 'iphone'},
+    ]
+};
+console.log(db);
 //schema decribes the data structure
 //tells how the user object looks like
 const UserType = new GraphQLObjectType({
@@ -24,7 +32,7 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: {id: {type: GraphQLString}},
             resolve(parentValue, args) {
-                return _.find(users, {id: args.id});
+                return _.find(db.users, {id: args.id});
             }
         }
         
